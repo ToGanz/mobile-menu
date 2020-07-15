188f6abd732093aada29005db6ca141083154b13
tganz <tganz5890@googlemail.com>
2020-07-15 07:40:01 +0200

    adds hamburger menu

---
+++ src/mobileMenu.js
@@ -0,0 +1,12 @@
+const mobileMenu = () => {
+  const navButton = document.querySelector("button[aria-expanded]");
+
+  function toggleNav({ target }) {
+    const expanded = target.getAttribute("aria-expanded") === "true" || false;
+    navButton.setAttribute("aria-expanded", !expanded);
+  }
+
+  navButton.addEventListener("click", toggleNav);
+};
+
+export default mobileMenu;
